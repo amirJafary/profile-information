4d25d4ded7fca319aeae83879760d5fcefe1fee9
sheikh jafari <amirjaffari300245@gmail.com>
2021-11-21 18:57:09 +0330

    first setting for start profile information page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css';
+import ProfileInformation from './view/profileInformation/ProfileInformation';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <ProfileInformation/>
+            </div>
+        )
+    }
+}
